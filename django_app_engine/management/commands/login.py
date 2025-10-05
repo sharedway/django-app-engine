956f7d680e16db0a7c285c49ee8da49a2bb8d933
Lauro C Oliveira <lauro@hostcert.com.br>
2025-10-05 13:30:47 -0300

    Refatorado startapp para utilizar template interno

--- django_app_engine/management/commands/login.py
+++ django_app_engine/management/commands/login.py
@@ -13,9 +13,11 @@ from django.utils.translation import gettext as _
 from django.utils.translation import gettext_lazy as _lazy
 from django.utils import translation
 
+MANAGEMENT_BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 class Command(BaseCommand):
     help = _("Login to api Manager")
 
     def handle(self, **options):
-        pass
+        print(MANAGEMENT_BASE_DIR)
